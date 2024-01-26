b1e102e752d5188a2c4fd5597bbe49f235fae27c
stefanjoseph <stefanjc2@gmail.com>
2024-01-26 00:01:54 -0800

    Allowed TrackManager to accept fallen objects as new obstacles

--- ggj2024/Assets/Scripts/Stefan Scripts/TrackManager.cs
+++ ggj2024/Assets/Scripts/Stefan Scripts/TrackManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TrackManager : MonoBehaviour
 {
     public GameObject obstacle;
+    public GameObject fallingObstacle;
+    public List<GameObject> fallingObstacles = new();
     public List<GameObject> fallenObstacles = new();
     public float visibilityDistance;
     public float exitEdgePosition = 0.0f;
@@ -14,6 +16,9 @@ public class TrackManager : MonoBehaviour
     public GameObject exitVantagePoint;
     public GameObject frontVantagePoint;
     public Vector2 unitScale;
+    public bool shouldDropObjects;
+    public float SPAWN_HEIGHT;
+    public float TREAD_HEIGHT;
 
     // Start is called before the first frame update
     void Start()
@@ -39,7 +44,7 @@ public class TrackManager : MonoBehaviour
             if (IsObstacleWithinVisibleWindow(obstacleComponent))
             {
                 thisObstacle.gameObject.SetActive(true);
-                Vector3 newPosition = DeterminePositionUsingVantagePoint(ConvertToNearPlaneOffset(obstacleComponent.relativePosition));
+                Vector3 newPosition = DetermineAbsolutePositionUsingVantagePoint(ConvertToExitEdgeOffset(obstacleComponent.relativePosition));
                 thisObstacle.transform.position = new Vector3(newPosition.x, newPosition.y, newPosition.z);
             }
             else
@@ -48,7 +53,21 @@ public class TrackManager : MonoBehaviour
             }
         }
 
-        ManuallyCreateObstacle();
+        for (int i = 0; i < fallingObstacles.Count; i++)
+        {
+            GameObject thisObstacle = fallingObstacles[i];
+            CheckForObstacleFreezes(thisObstacle);
+        }
+
+        if (shouldDropObjects)
+        {
+            ManuallyDropObstacle();
+        }
+        else
+        {
+            ManuallyCreateStaticObstacle();
+        }
+        
     }
 
     public bool IsObstacleWithinVisibleWindow(Obstacle obstacle)
@@ -70,7 +89,7 @@ public class TrackManager : MonoBehaviour
         return position % 1.0f;
     }
 
-    public Vector2 ConvertToNearPlaneOffset(float trackPositionX, float trackPositionY)
+    public Vector2 ConvertToExitEdgeOffset(float trackPositionX, float trackPositionY)
     {
         float nearPlaneOffsetX = trackPositionX - this.exitEdgePosition;
 
@@ -82,36 +101,95 @@ public class TrackManager : MonoBehaviour
         return new Vector2(nearPlaneOffsetX, nearPlaneOffsetY);
     }
 
-    public Vector2 ConvertToNearPlaneOffset(Vector2 trackPosition)
+    public Vector2 ConvertToExitEdgeOffset(Vector2 trackPosition)
     {
-        return ConvertToNearPlaneOffset(trackPosition.x, trackPosition.y);
+        return ConvertToExitEdgeOffset(trackPosition.x, trackPosition.y);
     }
 
-    public Vector3 DeterminePositionUsingVantagePoint(Vector2 nearPlaneOffset)
+    public Vector3 DetermineAbsolutePositionUsingVantagePoint(Vector2 exitEdgeOffset)
     {
         // From the perspective of the exit vantage point, we are facing the positive z direction
         // Negative x is to our left and positive x is to our right
-        float z = this.exitVantagePoint.transform.position.z + unitScale.x*nearPlaneOffset.x;
-        float x = this.exitVantagePoint.transform.position.x + unitScale.y*nearPlaneOffset.y;
+        float z = this.exitVantagePoint.transform.position.z + unitScale.x*exitEdgeOffset.x;
+        float x = this.exitVantagePoint.transform.position.x + unitScale.y*exitEdgeOffset.y;
 
         // Debug.Log($"finalX:{z} finalY:{x}");
         return new Vector3(x, 0.0f, z);
     }
 
-    public void CreateObstacle(float x, float y)
+    public Vector2 DetermineExitEdgeOffsetUsingVantagePoint(Vector3 absolutePosition)
     {
-        GameObject newObstacle = Instantiate(obstacle, DeterminePositionUsingVantagePoint(ConvertToNearPlaneOffset(x, y)), Quaternion.identity);
+        Vector2 exitEdgeOffset = new()
+        {
+            // From the perspective of the exit vantage point, we are facing the positive z direction
+            // Negative x is to our left and positive x is to our right
+            x = (absolutePosition.z - this.exitVantagePoint.transform.position.z) / unitScale.x,
+            y = (absolutePosition.x - this.exitVantagePoint.transform.position.x) / unitScale.y
+        };
+
+        return exitEdgeOffset;
+    }
+
+    public Vector2 ConvertToRelativePosition(Vector2 exitEdgeOffset)
+    {
+        float trackPositionX = exitEdgeOffset.x + this.exitEdgePosition;
+
+        trackPositionX = trackPositionX > 1.0f ? trackPositionX - 1.0f : trackPositionX;
+
+        float trackPositionY = exitEdgeOffset.y + 0.5f;
+
+        return new Vector2(trackPositionX, trackPositionY);
+    }
+
+    public void ConvertToStaticObstacle(GameObject obstacle)
+    {
+        obstacle.GetComponent<Rigidbody>().useGravity = false;
+        obstacle.GetComponent<Rigidbody>().isKinematic = false;
+
+        obstacle.GetComponent<Obstacle>().relativePosition = ConvertToRelativePosition(DetermineExitEdgeOffsetUsingVantagePoint(obstacle.transform.position));
+
+        this.fallenObstacles.Add(obstacle);
+    }
+
+    public void CheckForObstacleFreezes(GameObject obstacle)
+    {
+        if (obstacle.transform.position.y <= TREAD_HEIGHT)
+        {
+            fallingObstacles.Remove(obstacle);
+            ConvertToStaticObstacle(obstacle);
+        }
+    }
+
+    public void CreateStaticObstacle(float x, float y)
+    {
+        GameObject newObstacle = Instantiate(obstacle, DetermineAbsolutePositionUsingVantagePoint(ConvertToExitEdgeOffset(x, y)), Quaternion.identity);
 
         newObstacle.GetComponent<Obstacle>().relativePosition = new(x, y);
 
         this.fallenObstacles.Add(newObstacle);
     }
 
-    public void ManuallyCreateObstacle()
+    public void ManuallyCreateStaticObstacle()
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            CreateObstacle(this.exitEdgePosition, 0.5f);
+            CreateStaticObstacle(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
         }
     }
+
+    public void ManuallyDropObstacle()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            float x = Random.Range(0.0f, visibilityDistance);
+            float y = Random.Range(0.0f, 1.0f);
+
+            Vector3 spawnPointWithoutHeight = DetermineAbsolutePositionUsingVantagePoint(ConvertToExitEdgeOffset(x, y));
+
+            GameObject droppedObstacle = Instantiate(fallingObstacle, new Vector3(spawnPointWithoutHeight.x, SPAWN_HEIGHT, spawnPointWithoutHeight.z), Random.rotation);
+            
+            fallingObstacles.Add(droppedObstacle);
+        }
+    }
+
 }
